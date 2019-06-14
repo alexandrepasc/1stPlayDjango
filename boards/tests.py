f52e8d2856659f942da3b2ad16851fe23773510e
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-14 12:07:48 +0100

    Add new test

--- boards/tests.py
+++ boards/tests.py
@@ -1,8 +1,14 @@
 from django.urls import reverse, resolve
+from django.test import TestCase
 
 
-class TestUrls:
+class TestUrls(TestCase):
 
     def test_home_url(self):
         path = reverse('home')
         assert resolve(path).view_name == 'home'
+
+    def test_home_status(self):
+        path = reverse('home')
+        response = self.client.get(path)
+        assert response.status_code == 200
