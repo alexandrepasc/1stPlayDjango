2e86dfb5b3301ea45104201dadc53cd39a2f3317
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-16 18:15:28 +0100

    Add signup view, url, navbar and page. Add attribute 'breadcrumb' on view to fill the breadcrumb and title block.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request):
+    setattr(request, 'view', 'signup')
+    setattr(request, 'breadcrumb', 'Sign up')
+
+    return render(request, 'signup.html')
