96b985631804722e36988c2c125ae769f99de632
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-16 21:56:10 +0100

    Implement the form to the view and page.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 
 def signup(request):
     setattr(request, 'view', 'signup')
     setattr(request, 'breadcrumb', 'Sign up')
+    setattr(request, 'submitName', 'Sign')
 
-    return render(request, 'signup.html')
+    form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
