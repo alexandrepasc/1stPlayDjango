8dd6b9a16b04d8bb26a1dce177bac3e7dde00806
alexandrepasc <alexandre.pascoal@gmail.com>
2019-06-17 10:33:27 +0100

    Implement the user creation.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def signup(request):
@@ -7,6 +8,14 @@ def signup(request):
     setattr(request, 'breadcrumb', 'Sign up')
     setattr(request, 'submitName', 'Sign')
 
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
 
     return render(request, 'signup.html', {'form': form})
